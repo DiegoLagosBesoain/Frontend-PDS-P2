811878e9c97ef6d89270461cb95a516a8ffe1bd1
DiegoLagosBesoain <dnlagos@miuandes.cl>
2025-09-30 23:21:44 -0300

    arreglando ruta api

--- src/forms/CreateElementModal.jsx
+++ src/forms/CreateElementModal.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-const API_URL = "http://localhost:4000/api";
+const API_URL = import.meta.env.VITE_API_URL || "";;
 
 export default function CreateElementModal({
   project_id,
