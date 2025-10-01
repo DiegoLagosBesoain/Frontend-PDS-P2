088747d9482e5372a36e129b128b7e8d5c682be3
DiegoLagosBesoain <dnlagos@miuandes.cl>
2025-09-30 23:12:24 -0300

    arreglando viteconfig2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: [
-      "frontend-pds-p2.onrender.com"
-    ]
+    host: true, // permite 0.0.0.0
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ['frontend-pds-p2.onrender.com'] // tu dominio de Render
   }
 })
